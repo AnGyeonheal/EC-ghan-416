16f804bba445998a7844075ca7c8abd078c83041
HGU_GH <hilla0806@naver.com>
2023-10-20 02:04:23 +0900

    update

--- tutorial/TU_PWM/TU_Timer_PWM_student.c
+++ tutorial/TU_PWM/TU_Timer_PWM_student.c
@@ -26,34 +26,40 @@ int main(void) {
 	
 	// GPIO: ALTERNATIVE function setting
 	GPIOA->AFR[0] |= 1UL << (4*LED_PIN);  // AF1 at PA5 = TIM2_CH1 (p.150)
-	
-	// TIMER: PWM setting
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;          	// Enable TIMER clock
+	GPIOA->MODER &= ~(3UL << 2*LED_PIN);
+    GPIOA->MODER |= 2UL << 2*LED_PIN;
+
+    // TIMER: PWM setting
+	RCC->APB1ENR |= 1 << 0;         // Enable TIMER clock
 	
 	TIMx->CR1 &= ~(1UL << 4UL);				    // Direction Up-count
 	
-	TIMx->PSC = 839UL;						          // Set Timer CLK = 100kHz : (PSC + 1) = 84MHz/100kHz --> PSC = ?
+	TIMx->PSC = 839UL;						    // Set Timer CLK = 100kHz : (PSC + 1) = 84MHz/100kHz --> PSC = ?
 	
-	TIMx->ARR = 99UL;							       	  // Auto-reload: Upcounting (0...ARR). 
-																				// Set Counter CLK = 1kHz : (ARR + 1) = 100kHz/1kHz --> ARR = ?
+	TIMx->ARR = 99UL;							// Auto-reload: Upcounting (0...ARR).
+												// Set Counter CLK = 1kHz : (ARR + 1) = 100kHz/1kHz --> ARR = ?
 	
 	TIMx->CCMR1 &= ~TIM_CCMR1_OC1M;  			// Clear ouput compare mode bits for channel 1
 	TIMx->CCMR1 |= 6UL << 4UL;    				// OC1M = 110 for PWM Mode 1 output on ch1 = 
-	TIMx->CCMR1	|= TIM_CCMR1_OC1PE;    		// Output 1 preload enable (make CCR1 value changable)
+	TIMx->CCMR1	|= 1UL << 3UL;    		    // Output 1 preload enable (make CCR1 value changable)
 	
-	TIMx->CCR1 =  (99 + 1) / 2;     			// Output Compare Register for channel 1 	
+	TIMx->CCR1 =  (TIMx->ARR + 1) / 2;     			// Output Compare Register for channel 1
 	
-	TIMx->CCER &= ~TIM_CCER_CC1P;    			// select output polarity: active high	
-	TIMx->CCER |= 1UL << 0UL;							// Enable output for ch1
+	TIMx->CCER &= ~(1UL << 1UL);    			// select output polarity: active high
+	TIMx->CCER |= 1UL << 0UL;					// Enable output for ch1
 	
 	TIMx->CR1  |= TIM_CR1_CEN;      			// Enable counter
-	
+	TIMx->BDTR |= TIM_BDTR_MOE;
 	// Inifinite Loop ----------------------------------------------------------
 	while(1){
-		for (uint32_t i=0;i<10;i++){
-		TIM2->CCR1 = i * 1000;
-		delay_ms(100);
+		for (uint32_t i=0;i<=10;i+=5){
+            TIMx->CCR1 = i * 10;
+            delay_ms(100);
 		}
+        for (uint32_t i=0;i<=10;i+=5){
+            TIMx->CCR1 = (10-i) * 10;
+            delay_ms(100);
+        }
 	}
 }
 // Initialiization 
@@ -65,5 +71,4 @@ void setup(void)
 	GPIO_pupd(GPIOA, LED_PIN, EC_NONE);
 	GPIO_otype(GPIOA, LED_PIN, EC_PUSH_PULL);
 	GPIO_ospeed(GPIOA, LED_PIN, EC_HIGH);
-	
 }
