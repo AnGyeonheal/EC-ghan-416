ff36ede5cb101bd6cac4dc2a66c1735f47d218e4
AnGyeonheal <118132313+AnGyeonheal@users.noreply.github.com>
2023-12-09 13:59:18 +0900

    update

--- lab/Final Project/Final_Excavator.c
+++ lab/Final Project/Final_Excavator.c
@@ -1,9 +1,11 @@
 #include "ecSTM32F411.h"
 
-PinName_t PWM_PIN = PA_0;
+PinName_t PWM_PIN1 = PA_0;
 #define DIR_PIN1 3
 PinName_t PWM_PIN2 = PA_1;
 #define DIR_PIN2 2
+PinName_t PWM_PIN3 = PB_10;
+#define DIR_PIN3  4
 
 void setup(void);
 
@@ -11,11 +13,15 @@ void TIM3_IRQHandler(void);
 
 uint32_t count = 0;
 float period = 1;
-float duty = 0.5;
+float duty1;
+float duty2;
+float duty3;
+
 int dir1 = 0;
 int dir2 = 0;
+int dir3 = 0;
 static int stop = 0;
-
+static volatile uint8_t BT_Data = 0;
 
 int main(void) {
     // Initialization --------------------------------------------------
@@ -23,23 +29,67 @@ int main(void) {
 
     // Infinite Loop ---------------------------------------------------
     while (1) {
+        // First
         GPIO_write(GPIOC, DIR_PIN1, dir1);
-        PWM_duty(PWM_PIN, duty);
+        PWM_duty(PWM_PIN1, duty1);
+        // Second
         GPIO_write(GPIOC, DIR_PIN2, dir2);
-        PWM_duty(PWM_PIN2, 1);
+        PWM_duty(PWM_PIN2, duty2);
+        // shovel
+        GPIO_write(GPIOC, DIR_PIN3, dir3);
+        PWM_duty(PWM_PIN3, duty3);
     }
 }
 
+void USART1_IRQHandler(){
+    if(is_USART1_RXNE()){
+        BT_Data = USART1_read();
+        // First
+        if(BT_Data == 'q'){
+            dir1 = 0;
+            duty1 = 1;
+            dir2 = 0;
+            duty2 = 0;
+        }
+        else if(BT_Data == 'a') {
+            dir1 = 1;
+            duty1 = 0;
+            dir2 = 0;
+            duty2 = 0;
+        }
+        // Second
+        else if(BT_Data == 'w') {
+            dir2 = 0;
+            duty2 = 1;
+            dir3 = 0;
+            duty3 = 0;
+        }
+        else if(BT_Data == 's') {
+            dir2 = 1;
+            duty2 = 0;
+            dir3 = 0;
+            duty3 = 0;
+        }
+        // Shovel
+        else if(BT_Data == 'e') {
+            dir3 = 0;
+            duty3 = 1;
+            dir2 = 0;
+            duty2 = 0;
+        }
+        else if(BT_Data == 'd') {
+            dir3 = 1;
+            duty3 = 0;
+            dir2 = 0;
+            duty2 = 0;
+        }
+    }
+}
+
+
 void TIM3_IRQHandler(void) {
     if (is_UIF(TIM3)) {            // Check UIF(update interrupt flag)
-        count++;
-        if(count > 2000 && count < 4000){
-            dir1 = 1;
-        }
-        else if(count > 4000){
-            dir1 = 0;
-            count = 0;
-        }
+
     }
     clear_UIF(TIM3);            // Clear UI flag by writing 0
 }
@@ -51,17 +101,31 @@ void setup(void) {
     // SYSTICK
     SysTick_init();
 
-    // DIR SETUP
+    // Bluetooth serial init
+    UART1_init();
+    UART1_baud(BAUD_9600);
+
+    // DIR1
     GPIO_init(GPIOC, DIR_PIN1, OUTPUT);
     GPIO_otype(GPIOC, DIR_PIN1, EC_PUSH_PULL);
+    // DIR2
     GPIO_init(GPIOC, DIR_PIN2, OUTPUT);
     GPIO_otype(GPIOC, DIR_PIN2, EC_PUSH_PULL);
+    // DIR3
+    GPIO_init(GPIOC, DIR_PIN3, OUTPUT);
+    GPIO_otype(GPIOC, DIR_PIN3, EC_PUSH_PULL);
+
     // TIM
     TIM_UI_init(TIM3, 1);
 
-    // PWM
-    PWM_init(PWM_PIN);
-    PWM_period_ms(PWM_PIN, period);
+    // PWM1
+    PWM_init(PWM_PIN1);
+    PWM_period_ms(PWM_PIN1, period);
+    // PWM2
     PWM_init(PWM_PIN2);
     PWM_period_ms(PWM_PIN2, period);
+    // PWM3
+    PWM_init(PWM_PIN3);
+    PWM_period_ms(PWM_PIN3, period);
+
 }
