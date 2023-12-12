57dd9ef5effa33f480767752c872c9507535b9ba
AnGyeonheal <118132313+AnGyeonheal@users.noreply.github.com>
2023-12-12 17:26:32 +0900

    update

--- lab/Final Project/Final_Excavator.c
+++ lab/Final Project/Final_Excavator.c
@@ -1,26 +1,38 @@
 #include "ecSTM32F411.h"
+// Functions
+void setup(void);
+void TIM3_IRQHandler(void);
 
+// ARM Part
 PinName_t PWM_PIN1 = PA_0;
 #define DIR_PIN1 3
 PinName_t PWM_PIN2 = PA_1;
 #define DIR_PIN2 2
 PinName_t PWM_PIN3 = PB_10;
 #define DIR_PIN3  4
-
-void setup(void);
-
-void TIM3_IRQHandler(void);
-
-uint32_t count = 0;
 float period = 1;
 float duty1;
 float duty2;
 float duty3;
-
 int dir1 = 0;
 int dir2 = 0;
 int dir3 = 0;
+
+// UltraSonic parameter define
+#define TRIG PA_6
+#define ECHO PB_6
+uint32_t ovf_cnt = 0;
+float distance = 0;
+float timeInterval = 0;
+float time1 = 0;
+float time2 = 0;
+
+// Variables
+char mode;
 static int stop = 0;
+uint32_t count = 0;
+
+// Bluetooth Data
 static volatile uint8_t BT_Data = 0;
 
 int main(void) {
@@ -29,6 +41,17 @@ int main(void) {
 
     // Infinite Loop ---------------------------------------------------
     while (1) {
+
+        if(mode == 'A'){
+            distance = (float) timeInterval * 340.0 / 2.0 / 10.0; 	// [mm] -> [cm]
+            while(distance > 10){
+                dir1 = 1;
+                duty1 = 0;
+                dir2 = duty2 = 0;
+                dir3 = duty3 = 0;
+            }
+
+        }
         // First
         GPIO_write(GPIOC, DIR_PIN1, dir1);
         PWM_duty(PWM_PIN1, duty1);
@@ -45,56 +68,68 @@ void USART1_IRQHandler(){
     if(is_USART1_RXNE()){
         BT_Data = USART1_read();
         // First
-        if(BT_Data == 'q'){
-            dir1 = 0;
-            duty1 = 1;
-            dir2 = 0;
-            duty2 = 0;
-            dir3 = 0;
-            duty3 = 0;
+
+        if (BT_Data == 'M'){
+            mode = 'M';
         }
-        else if(BT_Data == 'a') {
-            dir1 = 1;
-            duty1 = 0;
-            dir2 = 0;
-            duty2 = 0;
-            dir3 = 0;
-            duty3 = 0;
+        else if (BT_Data == 'A'){
+            mode = 'A';
         }
-        // Second
-        else if(BT_Data == 'w') {
-            dir2 = 0;
-            duty2 = 1;
-            dir3 = 0;
-            duty3 = 0;
-            dir1 = 0;
-            duty1 = 0;
+        if(mode == 'M'){
+            if(BT_Data == 'u'){
+                dir1 = 0;
+                duty1 = 1;
+                dir2 = duty2 = 0;
+                dir3 = duty3 = 0;
+            }
+            else if(BT_Data == 'j'){
+                dir1 = 1;
+                duty1 = 0;
+                dir2 = duty2 = 0;
+                dir3 = duty3 = 0;
+            }
+            else if(BT_Data == 'i'){
+                dir2 = 0;
+                duty2 = 1;
+                dir1 = duty1 = 0;
+                dir3 = duty3 = 0;
+            }
+            else if(BT_Data == 'k'){
+                dir2 = 1;
+                duty2 = 0;
+                dir1 = duty1 = 0;
+                dir3 = duty3 = 0;
+            }
+            else if(BT_Data == 'o'){
+                dir3 = 0;
+                duty3 = 1;
+                dir2 = duty2 = 0;
+                dir1 = duty1 = 0;
+            }
+            else if(BT_Data == 'l'){
+                dir3 = 1;
+                duty3 = 0;
+                dir2 = duty2 = 0;
+                dir1 = duty1 = 0;
+            }
         }
-        else if(BT_Data == 's') {
-            dir2 = 1;
-            duty2 = 0;
-            dir3 = 0;
-            duty3 = 0;
-            dir1 = 0;
-            duty1 = 0;
-        }
-        // Shovel
-        else if(BT_Data == 'e') {
-            dir3 = 0;
-            duty3 = 1;
-            dir2 = 0;
-            duty2 = 0;
-            dir1 = 0;
-            duty1 = 0;
-        }
-        else if(BT_Data == 'd') {
-            dir3 = 1;
-            duty3 = 0;
-            dir2 = 0;
-            duty2 = 0;
-            dir1 = 0;
-            duty1 = 0;
-        }
+    }
+}
+
+void TIM4_IRQHandler(void){
+    if(is_UIF(TIM4)){                     // Update interrupt
+        ovf_cnt++;													// overflow count	        // count for 1sec
+        clear_UIF(TIM4);  							    // clear update interrupt flag
+    }
+    if(is_CCIF(TIM4, 1)){ 								// TIM4_Ch1 (IC1) Capture Flag. Rising Edge Detect
+        time1 = TIM4->CCR1;									// Capture TimeStart
+        clear_CCIF(TIM4, 1);                // clear capture/compare interrupt flag
+    }
+    else if(is_CCIF(TIM4, 2)){ 									// TIM4_Ch2 (IC2) Capture Flag. Falling Edge Detect
+        time2 = TIM4->CCR2;									// Capture TimeEnd
+        timeInterval = ((time2 - time1) + (TIM4->ARR+1) * ovf_cnt) * 0.01; 	// (10us * counter pulse -> [msec] unit) Total time of echo pulse
+        ovf_cnt = 0;                        // overflow reset
+        clear_CCIF(TIM4,2);								  // clear capture/compare interrupt flag
     }
 }
 
@@ -130,6 +165,7 @@ void setup(void) {
     // TIM
     TIM_UI_init(TIM3, 1);
 
+    // ARM Part
     // PWM1
     PWM_init(PWM_PIN1);
     PWM_period_ms(PWM_PIN1, period);
@@ -139,5 +175,11 @@ void setup(void) {
     // PWM3
     PWM_init(PWM_PIN3);
     PWM_period_ms(PWM_PIN3, period);
+
+    // Input Capture configuration -----------------------------------------------------------------------
+    ICAP_init(ECHO);    	// PB_6 as input caputre
+    ICAP_counter_us(ECHO, 10);   	// ICAP counter step time as 10us
+    ICAP_setup(ECHO, 1, IC_RISE);  // TIM4_CH1 as IC1 , rising edge detect
+    ICAP_setup(ECHO, 2, IC_FALL);  // TIM4_CH2 as IC2 , falling edge detect
 
 }
